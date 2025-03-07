b281bcbda3d0d942f22aed61c5e62c3a8eb2862a
Marvin Mango <marvinke2001@gmail.com>
2025-03-07 07:31:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+// Declare companyName as a const and assign 'Scuber' to it
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood with let and assign 'Chelsea' to it
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo with let and assign 'Susan Smith' to it
+let companyCeo = 'Susan Smith';
+
+
